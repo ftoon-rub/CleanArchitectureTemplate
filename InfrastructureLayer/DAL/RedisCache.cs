cb9bcabf03f7a3ee0afccdfdc1f38dd4efc35f49
Ftoon Alrubayea <ftoon-rubayea@hotmail.com>
2024-10-28 18:32:34 +0300

    edit redis cache class name

--- InfrastructureLayer/DAL/RedisCache.cs
+++ InfrastructureLayer/DAL/RedisCache.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApplicationLayer.Interfaces.InfrastructureLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace InfrastructureLayer.DAL
 {
-    internal class Redis
+    public class RedisCache : IRedisCache
     {
     }
 }
