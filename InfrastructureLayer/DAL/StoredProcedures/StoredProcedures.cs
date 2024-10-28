772de22354ea5d66a6db12f28ef09a3b3ef23aac
Ftoon Alrubayea <ftoon-rubayea@hotmail.com>
2024-10-28 18:58:20 +0300

    add sp part

---
+++ InfrastructureLayer/DAL/StoredProcedures/StoredProcedures.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationLayer.Interfaces.InfrastructureLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLayer.DAL.StoredProcedures
+{
+    public class StoredProcedures : IStoredProcedures
+    {
+    }
+}
