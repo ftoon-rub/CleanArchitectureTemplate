b79cafdeeb973394ef456a35e4e68dc2c3b8aaff
Ftoon Alrubayea <ftoon-rubayea@hotmail.com>
2024-10-29 07:16:23 +0300

    remove un use namespaces

--- ApplicationLayer/Models/BaseModels/ErrorDetails.cs
+++ ApplicationLayer/Models/BaseModels/ErrorDetails.cs
@@ -1,9 +1,4 @@
 ﻿using ApplicationLayer.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ApplicationLayer.Models.BaseModels
 {
